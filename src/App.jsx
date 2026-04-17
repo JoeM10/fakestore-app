a0d8f23952c9b34168f39d51564e710074b2e823
JoeM10 <joemcdaniel1592@gmail.com>
2026-04-16 22:06:18 -0700

    Initial Commit. Basic file structure construction.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+
+    return (
+        <div className="container text-center">
+            <h1>Welcome to the Online Store Homepage!</h1>
+        </div>
+    )
+}
